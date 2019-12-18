e83b65068c8efbd019255195e97ac90eafba3cd4
dkgreen24 <14611107+dkgreen24@users.noreply.github.com>
2019-12-18 14:14:01 -0500

    changed default fig sizes

--- knitr_globalopts.R
+++ knitr_globalopts.R
@@ -1,14 +1,6 @@
-#define global knitr options
-#really helpful for documentation!
+#define global knitr options - really helpful for documentation!
 
-```{r global_options, include=FALSE}
-knitr::opts_chunk$set(fig.width=12, fig.height=8, fig.path='Figs/',
+```{r, global_options, include=FALSE}
+knitr::opts_chunk$set(fig.width=10, fig.height=8, fig.path='Figs/',
                       echo=FALSE, warning=FALSE, message=FALSE)
-```
-
-#this code helps define package options; useful I'm sure 
-
-```{r load_packages, include=FALSE}
-#instaill.packages(broman)
-library(broman)
 ```
