aecf1821d95750ee16f9fb9bc30f44756a4acbb6
dkgreen24 <14611107+dkgreen24@users.noreply.github.com>
2019-12-18 14:25:34 -0500

    deleted files

--- mutate2_func.R
+++
@@ -1,7 +0,0 @@
-mutate2 <- function(data, .vars, .funs) {
-    stopifnot(length(.vars) == length(.funs))
-    for (i in seq_along(.vars)) {
-        data <- mutate_at(data, .vars[[i]], .funs[[i]])
-    }
-    data
-}
